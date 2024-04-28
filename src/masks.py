d1bc191f168de20dfe06a97c2764a345d15b2a94
Woeyshi <zhenek19900101@gmail.com>
2024-04-28 12:49:43 +0300

    .

--- src/masks.py
+++ src/masks.py
@@ -1,15 +1,16 @@
-def mask_credit_card(card_number):
+def mask_card_number(card_number):
+
     masked_number = (
         card_number[:4]
         + " "
         + card_number[4:6]
-        + "** **"
-        + " "
+        + "** **** "
         + card_number[-4:]
     )
     return masked_number
 
 
-def mask_bank_account(account_number):
+def mask_account_number(account_number):
+
     masked_number = "**" + account_number[-4:]
     return masked_number
--- tests/masks_tests.py
+++ tests/masks_tests.py
@@ -1,9 +1,9 @@
-from src.masks import mask_bank_account, mask_credit_card
+from src.masks import mask_account_number, mask_card_number
 
 card_number = "7000792289606361"
-masked_card = mask_credit_card(card_number)
-print(masked_card)
+masked_card_number = mask_card_number(card_number)
+print(masked_card_number)
 
 account_number = "73654108430135874305"
-masked_account = mask_bank_account(account_number)
-print(masked_account)
+masked_account_number = mask_account_number(account_number)
+print(masked_account_number)
