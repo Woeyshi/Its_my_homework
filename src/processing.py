b9f8bbc5022b2c6c7df059263db8c3584c703eb3
Woeyshi <zhenek19900101@gmail.com>
2024-05-06 23:51:24 +0300

    pytest tests

--- src/processing.py
+++ src/processing.py
@@ -3,14 +3,14 @@ from typing import Dict, List
 
 def filter_by_state(dict_list: List[Dict], state: str = "EXECUTED") -> List[Dict]:
     """
-    Функция фильтрует словари в данном списке, оставляя только те словари, у которых значение ключа "state".
+Функция фильтрует словари в данном списке, оставляя только те словари, у которых значение ключа "state".
     """
     return [item for item in dict_list if item.get("state") == state]
 
 
 def sort_dict_list_by_date(dict_list: List[Dict[str, object]], order: str = "desc") -> List[Dict[str, object]]:
     """
-    Функция сортирует словари в данном списке в соответствии с ключом "date", преобразуя дату в строку перед сравнением.
+Функция сортирует словари в данном списке в соответствии с ключом "date", преобразуя дату в строку перед сравнением.
     """
 
     sorted_list = sorted(dict_list, key=lambda x: str(x["date"]), reverse=(order.lower() == "desc"))
--- tests/masks_tests.py
+++ tests/masks_tests.py
@@ -1,6 +1,26 @@
+import pytest
+
 from src.masks import mask_bank_account, mask_credit_card
 
-credit_card_number = "Visa Platinum 7000 7922 8960 6361"
+
+@pytest.fixture
+def number() -> str:
+    return "1234567890"
+
+
+def pytest_mask_bank_account(number: str) -> None:
+    assert mask_bank_account(number) == "**7890"
+
+
+@pytest.mark.parametrize(
+    "number_card, mask_number_card",
+    [("0987654334567890", "0987 65** **** 7890"), ("0000111122223333", "0000 11** **** 3333")],
+)
+def pytest_masked_card(number_card: str, mask_number_card: str) -> None:
+    assert mask_credit_card(number_card) == mask_number_card
+
+
+credit_card_number = "7000792289606361"
 masked_credit_card = mask_credit_card(credit_card_number)
 print(masked_credit_card)
 
